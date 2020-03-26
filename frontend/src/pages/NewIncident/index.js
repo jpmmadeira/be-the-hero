d4f211ca8fba9530961681fd8c96f6deb624d8f1
joao-madeira <joaomadeira@outlook.pt>
2020-03-26 23:54:24 +0000

    Frontend Finished and working with backend

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 
+import api from '../../services/api';
 import './styles.css';
 
 import logoImg from '../../assets/logo.svg';
 
 export default function NewIncident() {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [value, setValue] = useState('');
+
+    const history = useHistory();
+
+    const ongID = localStorage.getItem('ongId');
+
+    async function handleNewIncident(e) {
+        e.preventDefault();
+
+        const data = {
+            title,
+            description,
+            value
+        };
+
+        try {
+            await api.post('incidents', data, {
+                headers: {
+                    Authorization: ongID,
+                }
+            });
+
+            history.push('/profile');
+        } catch (error) {
+            alert('Erro ao registar novo caso, tente novamente.');
+        }
+    }
+
     return (
         <div className="new-incident-container">
             <div className="content">
@@ -15,18 +46,30 @@ export default function NewIncident() {
                     <h1>Registar novo caso</h1>
                     <p>Descreva o caso detalhadamente para encontrar um herói para o resolver.</p>
 
-                    <Link className="back-link" to="/">
-                        <FiArrowLeft size={16} color="E02041" />
+                    <Link className="back-link" to="/profile">
+                        <FiArrowLeft size={16} color="#E02041" />
                         Voltar para home
                     </Link>
 
                 </section>
 
-                <form>
-                    <input placeholder="Título do Caso" />
-                    <textarea placeholder="Descrição" />
-                    <input placeholder="Valor em euros" />
-                    <button>Registar</button>
+                <form onSubmit={handleNewIncident}>
+                    <input
+                        placeholder="Título do Caso"
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
+                    />
+                    <textarea
+                        placeholder="Descrição"
+                        value={description}
+                        onChange={e => setDescription(e.target.value)}
+                    />
+                    <input
+                        placeholder="Valor em euros"
+                        value={value}
+                        onChange={e => setValue(e.target.value)}
+                    />
+
                     <button className="button">Registar</button>
                 </form>
             </div >
