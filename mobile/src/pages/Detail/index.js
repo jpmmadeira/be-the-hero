9ab964fc0662419590b307347fbf4db7712a3b4d
joao-madeira <joaomadeira@outlook.pt>
2020-03-28 13:42:56 +0000

    Mobile with list working

---
+++ mobile/src/pages/Detail/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View } from 'react-native';
+import { render } from 'react-dom';
+
+export default function Detail() {
+    return (
+        <View />
+    );
+}
